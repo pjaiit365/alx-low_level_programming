e6efa9fd1dd2a4aee0199abc3b2b69cc8d6be1e4
Ebenezer Ayitey <pjaiitey@gmail.com>
2022-06-02 21:49:36 -0700

    Low level programming

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,8 @@
 #include <stdio.h>
-
 /**
- *
- *  * main - main block
- *
- *   * Return: 0
- *
- *    */
-
+ * main - main block
+ * Return: 0
+ */
 int main(void)
 
 {
@@ -15,5 +10,4 @@ int main(void)
 		printf("with proper grammar, but the outcome is a piece of art,\n");
 
 			return (0);
-
 }
