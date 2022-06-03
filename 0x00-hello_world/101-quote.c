6d9b44af535b921b218adfac7eb5aeb0ea76f32a
Ebenezer Ayitey <pjaiitey@gmail.com>
2022-06-02 21:55:10 -0700

    Low level programming

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,14 +2,10 @@
 #include <unistd.h>
 
 /**
+ * main - Entry point
+ * Return: Always 1
  *
- *  *main - Entry point
- *
- *   *Return: Always 1
- *
- *    *
- *
- *     */
+ */
 
 int main(void)
 {
