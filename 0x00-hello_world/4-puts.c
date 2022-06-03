7350ab1615ec43459c9c64a960e1695e809c7779
Ebenezer Ayitey <pjaiitey@gmail.com>
2022-06-02 21:47:16 -0700

    Low level programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,10 @@
 #include <stdio.h>
-
 /**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+ *  main - main block
+ *  Return: 0
+ */
 int main(void)
 {
-		puts("\"Programmin is like building a multilingual puzzle");
-
+		puts("\"Programming is like building a multilingual puzzle");
 			return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
+
 /**
  *
- *  * main - Entry point
+ *  * main - main block
  *
- *   *
+ *   * Return: 0
  *
- *    * Return: Always 0 (Success)
- *
- *     */
+ *    */
+
 int main(void)
+
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+
+			return (0);
+
 }
