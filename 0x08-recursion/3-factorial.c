cf1a01ef46c49529c349db4eef2ba42fa6258914
pjaiit365 <56177177+pjaiit365@users.noreply.github.com>
2022-06-24 00:25:09 +0000

    Recursion in C

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * factorial- compute factorial
+ * @n: number to compute factorial of
+ * Return: factorial of n
+ */
+int factorial(int n)
+{
+	if (n == 0)
+		return (1);
+	else if (n < 0)
+		return (-1);
+	return (n * factorial(n - 1));
+}
