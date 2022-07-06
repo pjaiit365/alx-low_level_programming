8f2fd5f6db36fb1e7c38984ae247e81cd634ba20
pjaiit365 <56177177+pjaiit365@users.noreply.github.com>
2022-07-06 00:17:01 +0000

    Preprocessors in C

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints file name of the program
+ * followed by a new line
+ * Return: Always zero
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
